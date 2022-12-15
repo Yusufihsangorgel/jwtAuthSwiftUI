0f75a56cb6889d6a94d182789f8c83fd8feba871
Yusufihsangorgel <87409084+Yusufihsangorgel@users.noreply.github.com>
2022-12-15 15:54:26 +0300

    Initial Commit

---
+++ jwtAuthSwiftUI/jwtAuthSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  jwtAuthSwiftUIApp.swift
+//  jwtAuthSwiftUI
+//
+//  Created by Yusuf İhsan Görgel on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct jwtAuthSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
